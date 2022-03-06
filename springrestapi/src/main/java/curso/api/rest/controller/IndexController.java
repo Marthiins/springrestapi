e097fb82f0ce262f58cf866b6fd7c57d621dc6b9
Marthiins <sergio.marthiins@gmail.com>
2022-03-06 11:10:57 -0300

    Retornando usuário em JSON para a tela

--- springrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ springrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,11 +1,15 @@
 package curso.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
 
 @RestController /* Obrigatorio Anotação para aceitar os metodos rest */
 @RequestMapping(value = "/usuario")
@@ -17,17 +21,26 @@ public class IndexController {
 
 	/* Serviço Restufull */
 	@GetMapping(value = "/", produces = "application/json") /* Para acessar pela URL é um get */
-	public ResponseEntity init(
-			@RequestParam(value = "nome", required = true, defaultValue = "Nome não informado") String nome, @RequestParam(value = "salario") Long salario) {
-		/*
-		 * Sempre utilizar ResponseEntity nos metodos 
-		 * * Passando varios parametros com nome e valor
-		 */
-
-		System.out.println("Parametro sendo recebido" + nome);
-
-		return new ResponseEntity("Ola Usuario REST spring boot seu nome é : " + nome + " salario é "+ salario,
-				HttpStatus.OK);
+	public ResponseEntity<Usuario> init(){
+		Usuario usuario = new Usuario();
+		usuario.setId(50L); //Id é Long
+		usuario.setLogin("seergio@gmail.com");
+		usuario.setNome("Sérgio");
+		usuario.setSenha("1234");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(10L); //Id é Long
+		usuario2.setLogin("seo@gmail.com");
+		usuario2.setNome("Kamylla");
+		usuario2.setSenha("1234");
+		
+		/*Como tem mais de 2 usuariocria a lista*/
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);//adicionar os usuarios na lista
+		usuarios.add(usuario2);
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK );
+				
 	}
 			
 }

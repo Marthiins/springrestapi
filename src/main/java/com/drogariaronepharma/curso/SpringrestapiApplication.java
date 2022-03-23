3b2af1b7a7190f049b1e55725b65c84a99f3a00f
marthiins <sergio.marthiins@gmail.com>
2022-03-22 22:15:44 -0300

    commit da aplicacao

--- src/main/java/com/drogariaronepharma/curso/SpringrestapiApplication.java
+++ src/main/java/com/drogariaronepharma/curso/SpringrestapiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc/*Habilitar a parte do mvc*/
 @RestController
 @EnableAutoConfiguration /*Para ter todas as anotações ativadas*/
-public class SpringrestapiApplication {
+public class SpringrestapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestapiApplication.class, args);

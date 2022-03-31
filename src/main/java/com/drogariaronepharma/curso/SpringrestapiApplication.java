073b32c0907b5223df0783795c9c78d417d20081
marthiins <sergio.marthiins@gmail.com>
2022-03-31 10:44:45 -0300

    deployd aplicacao

--- src/main/java/com/drogariaronepharma/curso/SpringrestapiApplication.java
+++ src/main/java/com/drogariaronepharma/curso/SpringrestapiApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = { "curso.api.rest.model" }) /* Criar as tabelas automaticas */
@@ -19,10 +22,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc/*Habilitar a parte do mvc*/
 @RestController
 @EnableAutoConfiguration /*Para ter todas as anotações ativadas*/
-public class SpringrestapiApplication extends SpringBootServletInitializer {
+public class SpringrestapiApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestapiApplication.class, args);
 	}
 
+	@CrossOrigin
+	/*Mapeamento global que refletem em todo o sistema*/
+	@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			
+		registry.addMapping("/usuario/**") 
+		.allowedMethods("*")
+		.allowedOrigins("*");
+		/*Liberando o mapeamento de usuario para todos as origens*/
+		}
 }
